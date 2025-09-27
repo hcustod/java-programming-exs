80bf40d02549adca9afdb2a17f46ecdcf79e6c77
hcustod <h.custodio.dev@gmail.com>
2025-09-27 14:12:19 -0400

    Start: Dir structure and basics (1-2)

---
+++ src/basics/Variables.java
@@ -0,0 +1,17 @@
+package basics;
+
+import java.awt.image.AreaAveragingScaleFilter;
+
+public class Variables {
+	public static void main(String[] args) {
+		int count = 3;
+		double price = 99.99;
+		boolean on = true;
+		char one = 'A';
+		String nameString = "Hank";
+		
+		System.out.printf("Hello my name is %s", nameString);
+		
+	}
+
+}
